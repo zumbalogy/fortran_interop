345c23b70d0582dcd9bf4fa0e3e742a5fdb71387
zumbalogy <intrapostal@gmail.com>
2017-10-23 00:11:53 -0400

    no implicit stuff in fortran

--- src/linux-x86-64/test.f90
+++ src/linux-x86-64/test.f90
@@ -1,11 +1,16 @@
 function main(a, b) ! bind(C, name='main') ! works without binding
-    integer, value :: a, b
-    integer :: mult
+  implicit none
+  integer, value :: a, b
+  integer :: main
+  integer :: f2
 
-    mult = f2(a) * b
+  main = f2(a) * b
 end function main
 
 function f2(a)
+  implicit none
   integer :: a
+  integer :: f2
+
   f2 = a + 1
 end function
